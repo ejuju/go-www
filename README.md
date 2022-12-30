eb3f44c50d8ff1f8df9febdd0f8e31fb7e3db092
ejuju <juju.sel.mail@gmail.com>
2022-12-30 21:42:21 +0100

    update readme and delete unused

---
+++ README.md
@@ -0,0 +1,12 @@
+# Web / HTTP utilities in Go
+
+Features:
+
+-   [x] Static website router (http.Handler)
+-   [x] Backend + frontend router (http.Handler)
+-   [x] Status & response body size recorder (http.ResponseWriter)
+-   [ ] CORS middleware
+-   [ ] Panic recovery middleware
+-   [ ] Logging middleware
+-   [ ] Rate limiting middleware
+-   [ ] Authorization middleware
--- website_handler.go
+++ website_handler.go
@@ -60,10 +60,6 @@ func NewWebsiteHandler(c WebsiteHandlerConfig) (http.Handler, error) {
 		return nil, fmt.Errorf("failed to get website build sub file-system: %w", err)
 	}
 
-	// debug: print file paths in fs
-	// p, err := paths(websiteFS)
-	// fmt.Printf("Printing website FS:\nErr: %v\nPaths: %v\n", err, fmtPaths(p))
-
 	// Get fallback page content
 	f, err := websiteFS.Open(c.FallbackPagePath)
 	if err != nil {
@@ -81,47 +77,3 @@ func NewWebsiteHandler(c WebsiteHandlerConfig) (http.Handler, error) {
 		fallbackHTML: fallbackPageData,
 	}, nil
 }
-
-// // Debug utils
-// func fmtPaths(paths map[string]int) string {
-// 	outputLines := []string{}
-// 	totalSize := 0
-// 	i := 1
-// 	for filepath, size := range paths {
-// 		fileKB := float64(size) / 1_000.0
-// 		line := fmt.Sprintf("%8.3f KB  %s\n", fileKB, filepath)
-// 		outputLines = append(outputLines, line)
-// 		totalSize += size
-// 		i++
-// 	}
-
-// 	// put html files first
-// 	sort.Slice(outputLines, func(i, j int) bool {
-// 		return strings.Contains(outputLines[i], ".html")
-// 	})
-
-// 	output := fmt.Sprintf("Website FS has %d files (total size: %.3f MB)\n", len(paths), float64(totalSize)/1_000_000.0)
-// 	for i, line := range outputLines {
-// 		output += fmt.Sprintf("\t%3d. %s", i+1, line)
-// 	}
-// 	return output
-// }
-
-// func paths(fsys fs.FS) (map[string]int, error) {
-// 	paths := map[string]int{}
-// 	err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if d.IsDir() {
-// 			return nil
-// 		}
-// 		info, err := d.Info()
-// 		if err != nil {
-// 			return err
-// 		}
-// 		paths[path] = int(info.Size())
-// 		return nil
-// 	})
-// 	return paths, err
-// }
