26ad111904a93f4d4a9cf58c13fceee7e5832571
ejuju <juju.sel.mail@gmail.com>
2022-12-30 21:48:42 +0100

    add json / text response utils

--- utils.go
+++ utils.go
@@ -1,8 +1,30 @@
 package www
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+const (
+	HeaderContentType    = "Content-Type"
+	ContentTypeJSON      = "application/json"
+	ContentTypePlainText = "text/plain; charset=utf-8"
+)
+
+// Writes a JSON response.
+func RespondJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set(HeaderContentType, ContentTypeJSON)
+	return json.NewEncoder(w).Encode(v)
+}
+
+// Writes a plain text response
+func RespondText(w http.ResponseWriter, status int, s string) error {
+	w.WriteHeader(status)
+	w.Header().Set(HeaderContentType, ContentTypePlainText)
+	_, err := w.Write([]byte(s))
+	return err
+}
 
 // Recorder implements the http.ResponseWriter interface.
 // It stores data about the current request / response and forwards it
